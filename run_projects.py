a29fe8faf8756ae1488c5d841f67a4b68482bc85
Thibault Saunier <thibault.saunier@collabora.com>
2013-09-15 14:37:09 -0300

    Rework timeout handling

--- run_projects.py
+++ run_projects.py
@@ -1,10 +1,13 @@
+import signal
 import subprocess32
 import time
 import os
 from optparse import OptionParser
 from buildgst import bcolors
-from gi.repository import GES, Gst
+from gi.repository import GES, Gst, GLib
 import xml.etree.ElementTree as ET
+
+DURATION_TOLERANCE = Gst.SECOND / 2
 
 formats = {"aac": "audio/mpeg,mpegversion=4",
            "ac3": "audio/x-ac3",
@@ -30,14 +33,14 @@ def print_ns(time):
 
 
 def indent(elem, level=0):
-    i = "\n" + level*"  "
+    i = "\n" + level * "  "
     if len(elem):
         if not elem.text or not elem.text.strip():
             elem.text = i + "  "
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for elem in elem:
-            indent(elem, level+1)
+            indent(elem, level + 1)
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
     else:
@@ -92,9 +95,9 @@ def get_profile(combination):
                             video_restriction="video/x-raw,format=I420")
 
 
-def getDuration(project):
+def getDuration(project_uri):
 
-    proj = GES.Project.new(project)
+    proj = GES.Project.new(project_uri)
     tl = proj.extract()
     if tl is None:
         duration = None
@@ -105,104 +108,155 @@ def getDuration(project):
     return 10 * 60
 
 
-def launch_project(project, combination, dest_dir, paths=[], elem=None):
+def launch_project(project_uri, combination, dest_uri, paths=[], elem=None):
     ret = True
 
+    if not Gst.uri_is_valid(dest_uri):
+        dest_uri = GLib.filename_to_uri(dest_uri, None)
+
     if combination is None:
-        msg = "Playing project", os.path.basename(project)
+        msg = "Playing project", os.path.basename(project_uri)
     else:
-        msg = "Rendering project %s to: %s" % (os.path.basename(project), combination)
+        msg = "Rendering project %s to: %s" % (os.path.basename(project_uri), combination)
 
     print bcolors.OKBLUE + len(msg) * '='
     print msg
     print len(msg) * '=' + bcolors.ENDC
 
-    dest_file = os.path.join(dest_dir, os.path.basename(project) +
+    dest_file = os.path.join(dest_uri, os.path.basename(project_uri) +
                              '-' + combination.acodec +
                              combination.vcodec + '.' +
                              combination.container)
     profile = get_profile(combination)
-    command = "ges-launch-1.0 -l %s -f %s -o %s " % (project, profile, dest_file)
+    command = "ges-launch-1.0 -l %s -f %s -o %s " % (project_uri, profile, dest_file)
     for path in paths:
         command += " -P %s" % path
 
-    duration = timeout = getDuration(project)
-    if combination:
-        timeout *= 3  # Give us 2 time the length of the timeline to render
-        print "Timeout set to %is" % timeout
-
+    duration =  getDuration(project_uri)
     if elem is not None:
         elem.attrib["classname"] = str(comb).replace(' ', '_')
-        elem.attrib["name"] = os.path.basename(project)
+        elem.attrib["name"] = os.path.basename(project_uri)
 
+    timeout = False
+    last_size = 0
+    last_change_ts = time.time()
     start = time.time()
-    stderr = open("sterr_o", 'wr')
-    try:
-        subprocess32.check_output(command, stderr=stderr, timeout=timeout, shell=True)
-        asset = GES.UriClipAsset.request_sync(dest_file)
-        if not asset:
-            err = ET.SubElement(elem, 'failure')
-            err.attrib["type"] = "Wrong rendered file"
-            tmpf = open("sterr_o", 'r')
-            err.text = "Rendered file could not be discovered\n %s" % tmpf.read()
-            tmpf.close()
-        elif asset.duration != duration:
-            err = ET.SubElement(elem, 'failure')
-            err.attrib["type"] = "Wrong rendered file"
-            tmpf = open("sterr_o", 'r')
-            err.text = "Rendered file as wrong duration (real: %s, expected %s)\n %s" \
-                       % (print_ns(asset.duration()),
-                          print_ns (duration),
-                          tmpf.read())
-            tmpf.close()
-    except subprocess32.TimeoutExpired as e:
-        msg = bcolors.FAIL, "TIMEOUT: running %s in %s:\n\n %s" % (command,
-              os.getcwd(), e.output), bcolors.ENDC
-        print msg
-        if elem is not None:
-            err = ET.SubElement(elem, 'failure')
-            err.attrib["type"] = "Timeout"
-            tmpf = open("sterr_o", 'r')
-            err.text = tmpf.read()
-            tmpf.close()
-        ret = False
-    except subprocess32.CalledProcessError as e:
-        msg = "\n\n%sFAILED to run project:%s with profile: %s:\n%s%s" \
-            % (bcolors.FAIL, project, profile, e.output, bcolors.ENDC)
-        print msg
-        if elem is not None:
-            err = ET.SubElement(elem, 'failure')
-            err.attrib["type"] = "Fail"
-            tmpf = open("sterr_o", 'r')
-            err.text = tmpf.read()
-            tmpf.close()
-        ret = False
+    fstderr = open("sterr_o", 'wr')
+    print "Launching: %s" % command
+    process = subprocess32.Popen(command, stderr=fstderr,
+                                 stdout=subprocess32.PIPE,
+                                 shell=True,
+                                 preexec_fn=os.setsid)
+    stdo, stde = None, None
+    while True:
+        try:
+            stdo, stde = process.communicate(timeout=1)
+        except subprocess32.TimeoutExpired:
+            pass
 
-    stderr.close()
+        if process.returncode is not None:
+            break
+
+        sz = os.stat(GLib.filename_from_uri(dest_file)[0]).st_size
+        if sz == last_size:
+            if time.time() - last_change_ts > 10:
+                print "10 seconds without any change in rendered file => timeout"
+                timeout = True
+                os.killpg(process.pid, signal.SIGTERM)
+                break
+        else:
+            last_change_ts = time.time()
+            last_size = sz
+
+        if stdo:
+            print stdo
+
+    if process.returncode == 0:
+        try:
+            asset = GES.UriClipAsset.request_sync(dest_file)
+            if duration - DURATION_TOLERANCE <= asset.get_duration() <= duration + DURATION_TOLERANCE:
+                err = ET.SubElement(elem, 'failure')
+                err.attrib["type"] = "Wrong rendered file"
+                tmpf = open("sterr_o", 'r')
+                err.text = "Rendered file as wrong duration (real: %s, expected %s)\n %s" \
+                           % (print_ns(asset.get_duration()),
+                              print_ns(duration),
+                              tmpf.read().replace(">", '_').replace('<', '_'))
+                tmpf.close()
+        except Exception as e:
+            err = ET.SubElement(elem, 'failure')
+            err.attrib["type"] = "Wrong rendered file"
+            tmpf = open("sterr_o", 'r')
+            err.text = "Rendered file could not be discovered: %s\n %s" % (e, tmpf.read())
+            tmpf.close()
+    else:
+        tmpf = open("sterr_o", 'r')
+        stderr = tmpf.read()
+        if timeout is True:
+            missing_eos = False
+            try:
+                asset = GES.UriClipAsset.request_sync(dest_file)
+                if asset.get_duration() == duration:
+                    missing_eos = True
+            except:
+                pass
+            print bcolors.FAIL, "TIMEOUT: running %s in %s:\n\n %s" % (command,
+                  os.getcwd(), stderr), bcolors.ENDC
+
+            if elem is not None:
+                err = ET.SubElement(elem, 'failure')
+                err.attrib["type"] = "Timeout"
+                if missing_eos is True:
+                    err.text = "The rendered file add right duration, MISSING EOS?\n\n" + \
+                            stderr
+                else:
+                    err.text = stderr
+            ret = False
+        else:
+            print "\n\n%sFAILED to run project:%s with profile: %s:\n%s%s" \
+                % (bcolors.FAIL, project_uri, profile, stderr, bcolors.ENDC)
+            if elem is not None:
+                err = ET.SubElement(elem, 'failure')
+                err.attrib["type"] = "Fail"
+                err.text = stderr
+            ret = False
+
+    tmpf.close()
+    fstderr.close()
     if elem is not None:
         elem.attrib["time"] = str(int(time.time() - start))
+    print "%sDone %s seconds: %i %s" %(bcolors.OKGREEN,
+                                       int(time.time() - start),
+                                       bcolors.ENDC)
 
     return ret
 
 
 if "__main__" == __name__:
     parser = OptionParser()
+    default_opath = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_VIDEOS)
+    if default_opath:
+        default_opath = os.path.join(default_opath, "ges-rendered-projects")
+    else:
+        default_opath = os.path.join(os.path.expanduser('~'), "Video",
+                "ges-rendered-projects")
     parser.add_option("-o", "--output-path", dest="dest",
-                      default="~/Videos/ges-rendered-projects",
+                      default=default_opath,
                       help="Set the path to which projects should be renderd")
     parser.add_option("-p", "--asset-path", dest="paths",
                       default=[],
                       help="Paths in which to look for moved assets")
     (options, args) = parser.parse_args()
 
-    options.dest = os.path.expanduser(os.path.abspath(options.dest))
-
     Gst.init(None)
     GES.init()
 
+    if not Gst.uri_is_valid(options.dest):
+        options.dest = GLib.filename_to_uri(options.dest, None)
+
     print "Creating directory: %s" % options.dest
     try:
-        os.makedirs(options.dest)
+        os.makedirs(GLib.filename_from_uri(options.dest)[0])
         print "Created directory: %s" % options.dest
     except OSError:
         pass
@@ -212,7 +266,7 @@ if "__main__" == __name__:
         if Gst.uri_is_valid(proj):
             projects.append(proj)
         else:
-            projects.append("file://" + proj)
+            projects.append(GLib.filename_to_uri(proj, None))
 
     if isinstance(options.paths, str):
         options.paths = [options.paths]
